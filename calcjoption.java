938dc294da9a408dc5a416c26956948725a1f2de
Juaokkkk <117794205+Juaokkkk@users.noreply.github.com>
2023-02-09 14:45:31 -0400

    Add files via upload

---
+++ calcjoption.java
@@ -0,0 +1,54 @@
+import javax.swing.DefaultBoundedRangeModel;
+import javax.swing.Icon;
+import javax.swing.JOptionPane;
+
+public class arquivoaula {
+    public static void main(String args[]) {
+
+        String num = JOptionPane.showInputDialog(null, "Digite um numero:");
+        String num2 = JOptionPane.showInputDialog(null, "Digite outro numero:");
+        String operacao = JOptionPane.showInputDialog(null, "Que operação deseja fazer");
+
+        int number = Integer.parseInt(num);
+        int number2 = Integer.parseInt(num2);
+        int resultado;
+        switch (operacao) {
+            case "somar":
+                resultado = number + number2;
+                JOptionPane.showMessageDialog(null, "o resultado é: " + resultado);
+        }
+
+        switch (operacao) {
+            case "+":
+                resultado = number + number2;
+                JOptionPane.showMessageDialog(null, "o resultado é: " + resultado);
+                break;
+
+            case "-":
+                resultado = number - number2;
+                JOptionPane.showMessageDialog(null, "o resultado é: " + resultado);
+                break;
+
+            case "*":
+                resultado = number * number2;
+                JOptionPane.showMessageDialog(null, "o resultado é: " + resultado);
+                break;
+
+            case "/":
+                resultado = number / number2;
+                JOptionPane.showMessageDialog(null, "o resultado é: " + resultado);
+                break;
+
+            default:
+                JOptionPane.showMessageDialog(null, "Digite um caracter valido.");
+        }
+
+        int message = JOptionPane.showConfirmDialog(null, "gostou?");
+
+        if (message == JOptionPane.YES_OPTION) {
+            JOptionPane.showMessageDialog(null, "obrigado");
+        } else {
+            JOptionPane.showMessageDialog(null, "affs");
+        }
+    }
+}
